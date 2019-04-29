05dc400e4225c1ad1b26044aeb01b008b7f22e4a
bh-lay <mail@bh-lay.com>
2019-04-29 19:12:36 +0800

    更新发布脚本

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,6 +10,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const ROOT_PATH = path.resolve(__dirname);
+const BUILD_PATH = path.resolve(ROOT_PATH, '../../bh-lay.com/static/build/moments/');
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -25,9 +27,10 @@ const webpackConfig = merge(baseWebpackConfig, {
   },
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
   output: {
-    path: config.build.assetsRoot,
+    path: BUILD_PATH,
     filename: utils.assetsPath('js/[name].[chunkhash].js'),
-    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
+    chunkFilename: utils.assetsPath('js/[id].[chunkhash].js'),
+    publicPath: 'http://static.bh-lay.com/build/moments/'
   },
   plugins: [
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
